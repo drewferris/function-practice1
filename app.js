3ba814b5d7721488cc770775de62d7fa12daaf39
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-03-18 09:33:57 -0700

    2nd question done

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ function sum (number1, number2) {
   var summed = number1 + number2;
   console.log('The sum of ' + number1 + ' and ' + number2 + ' is ' + summed + '.');
 }
+
+function multiply (number1, number2) {
+  var multiplied = number1 * number2;
+  console.log('The product of ' + number1 + ' and ' + number2 + ' is ' + multiplied + '.');
+}
