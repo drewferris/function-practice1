bd742670d63344029705f9470610c33140943246
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-03-18 10:12:02 -0700

    third question finished

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 function sum (number1, number2) {
   var summed = number1 + number2;
-  console.log('The sum of ' + number1 + ' and ' + number2 + ' is ' + summed + '.');
+  //console.log('The sum of ' + number1 + ' and ' + number2 + ' is ' + summed + '.');//
+  return summed;
 }
 
 function multiply (number1, number2) {
   var multiplied = number1 * number2;
-  console.log('The product of ' + number1 + ' and ' + number2 + ' is ' + multiplied + '.');
+  //console.log('The product of ' + number1 + ' and ' + number2 + ' is ' + multiplied + '.');
+  return multiplied;
+}
+
+function sumAndMultiply (number1, number2, number3){
+  var summedAgain = sum(number1, sum(number2, number3));
+  var multipliedAgain = multiply(number1, multiply(number2, number3));
+  console.log(number1 + ' and ' + number2 + ' and ' + number3 + ' sum to ' + summedAgain + '.');
+  console.log('The numbers ' + number1 + ' and ' + number2 + ' and ' + number3 + ' have a product of ' + multipliedAgain + '.');
+  return [summedAgain, multipliedAgain];
 }
